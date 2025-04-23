6ead96deb193b5338f92e3394d58f17b709e8781
Harooonsahil <haroonsahil671@gmail.com>
2025-04-24 03:21:57 +0500

    part3

---
+++ script.js
@@ -0,0 +1,135 @@
+// STRING METHODS
+
+// Trim method
+/*let full_name="Muhammad haroon  ";
+let trimed=full_name.trim();
+console.log(trimed);
+console.log(trimed.length);*/
+
+
+// To lower case 
+/*let full_name="Muhammad Haroon";
+let To_lower_case=full_name.toLowerCase();
+console.log(To_lower_case);*/
+
+
+//To upper case
+/*let full_name="Muhammad Haroon";
+let To_upper_case=full_name.toUpperCase();
+console.log(To_upper_case);*/
+
+
+
+// Methods with arguments
+// index of method
+/*let full_name="Muhammad Haroon";
+let index_of=full_name.indexOf('o');
+console.log(index_of);*/
+
+
+// Slice method
+/*let full_name="Muhammad Haroon";
+let sliced=full_name.slice(4,8);
+console.log(sliced);*/
+
+
+// Replace method
+/*let full_name="Muhammad Haroon khan";
+let Replaced=full_name.replace("khan","Jan");
+console.log(Replaced);*/
+
+
+
+// Repeat method
+/*let full_name="Muhammad Haroon";
+let Repeated=full_name.repeat(5);
+console.log(Repeated);*/
+
+
+
+// Trim and uppercase
+/*let full_name="Muhammad Haroon ";
+let updated=full_name.trim().toLocaleUpperCase();
+console.log(updated);*/
+
+
+
+// ARRAY WITH push method
+/*let std_Data=["Haroon",221430,12.9];
+std_Data.push("Younas");
+console.log(std_Data);*/
+
+
+// ARRAY WITH pop method
+/*let std_Data=["Haroon",221430,12.9];
+std_Data.pop();
+console.log(std_Data);*/
+
+
+
+// Array with shift and unshift method
+/*let std_Data=["Haroon",221430,12.9];
+std_Data.shift();
+std_Data.unshift("younas")
+console.log(std_Data);*/
+
+
+
+//start array and final array
+/*let start=["january","july","march","august"];
+start.shift();
+start.shift();
+start.unshift("june","july")
+console.log(start);*/
+
+
+// indexof method
+/*let std_Data=["Haroon",221430,12.9];
+let result=std_Data.indexOf(12.9);
+console.log(result);*/
+
+
+// include method
+/*let std_Data=["Haroon",221430,12.9];
+let result=std_Data.includes("younas");
+console.log(result);*/
+
+
+// array concatenation
+/*let months=["january","february","march"];
+let days=["monday","tuesday","wednesday","thursday"]
+let all=months.concat(days);
+console.log(all);*/
+
+
+
+// slice array
+/*let months=["january","february","march","april","may"];
+let sliced=months.slice(1,3);
+console.log(sliced);
+console.log(months);*/
+
+
+// sorting array
+/*let number=[1,3,2,4,2,8,3,2];
+number.sort();
+console.log(number);*/
+
+
+
+// splice array
+/*let number=[1,3,2,4,2,8,3,2];
+// for removing elements
+number.splice(0,3);
+
+// for adding element
+number.splice(2,4,6,2,4);
+
+//replacing elements
+number.splice(1,3,"yellow")
+console.log(number);*/
+
+
+
+
+
